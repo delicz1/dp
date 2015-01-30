990934f6a1ef647019277187c9426df80713a4e7
Deli <pes.hadra@seznam.cz>
2015-01-30 20:42:40 +0100

    bootstrap, jquery and update

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit3b5be746e3b67a2141061a461f7ada09::getLoader();
+return ComposerAutoloaderInit3d3485430f55f2780be404a2d21c1ae4::getLoader();
